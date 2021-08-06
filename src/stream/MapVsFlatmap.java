ed246133359fbfe8b2468cc1a39041fb922d89ee
i <25978246+luckygeo@users.noreply.github.com>
2021-08-06 16:01:01 +0800

    docs: add map vs flat map use

---
+++ src/stream/MapVsFlatmap.java
@@ -0,0 +1,41 @@
+package stream;
+
+import java.util.Arrays;
+import java.util.List;
+import java.util.Objects;
+import java.util.Optional;
+import java.util.stream.Collectors;
+import java.util.stream.Stream;
+
+/**
+ * @author: i
+ * @createDate: 2021/8/6
+ * @version: 1.0
+ * <p>
+ * map() and flatMap() APIs stem from functional languages. In Java 8, we can find them in Optional, Stream and in CompletableFuture (although under a slightly different name).
+ * <p>
+ * Streams represent a sequence of objects whereas optionals are classes that represent a value that can be present or absent. Among other aggregate operations, we have the map() and flatMap() methods.
+ * <p>
+ * Despite the fact that both have the same return types, they are quite different. Let's explain these differences by analyzing some examples of streams and optionals.
+ */
+public class MapVsFlatmap {
+    public static void main(String[] args) {
+        //--1. Map and Flatmap in Optionals
+        Optional<String> optional = Optional.of("test");
+        System.out.println(Objects.equals(Optional.of("TEST"), optional.map(String::toUpperCase)));
+        System.out.println(Objects.equals(Optional.of(Optional.of("STRING")),
+                Optional
+                        .of("string")
+                        .map(s -> Optional.of("STRING"))));
+        //flatmap can do it
+        System.out.println(Objects.equals(Optional.of("STRING"), Optional
+                .of("string")
+                .flatMap(s -> Optional.of("STRING"))));
+        //--2.Map and Flatmap in Streams
+        List<String> collect = Stream.of("a", "b").map(String::toUpperCase).collect(Collectors.toList());
+        collect.forEach(System.out::println);
+        List<List<String>> lists = Arrays.asList(Arrays.asList("a", "b"), Arrays.asList("c", "d"));
+        List<String> collect1 = lists.stream().flatMap(a -> a.stream()).map(String::toUpperCase).collect(Collectors.toList());
+        collect1.forEach(System.out::print);
+    }
+}
